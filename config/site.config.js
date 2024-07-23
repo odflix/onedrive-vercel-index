0585c2284b779687dfa07da8e2d7aeca42bb5ab2
odflix <54980077+odflix@users.noreply.github.com>
2024-07-24 00:44:59 +0530

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -19,6 +19,8 @@ module.exports = {
   // The name of your website. Present alongside your icon.
   title: "Kold Creative",
 
+  hideFilesMeta: ["/dead-site"],
+
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/Kold',
 

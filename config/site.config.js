3af992e0b9232bea810092927936c837d7f8b425
odflix <54980077+odflix@users.noreply.github.com>
2024-07-24 00:33:48 +0530

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ footer:
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/kold'],
+  protectedRoutes: ["/dead-site"],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:vi@2am.cool',

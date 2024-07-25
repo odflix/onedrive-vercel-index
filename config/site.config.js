aad44b9fe95a096c9dbf60f62fa61ca8dc983aaf
odflix <54980077+odflix@users.noreply.github.com>
2024-07-25 21:24:56 +0530

    Added footer text

--- config/site.config.js
+++ config/site.config.js
@@ -40,7 +40,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
 footer: 
-  '<p>This website is for people who can\'t afford it. Please support Sam Kolder\'s work by visiting <a href="https://www.koldercreative.com/pricing" target="_blank">www.koldercreative.com</a>.</p>',
+  '<p>This website is for people who can\'t afford it. Please support Sam Kolder\'s work by visiting <a href="https://www.koldercreative.com/pricing" target="_blank">www.koldercreative.com</a>. We donot own any of this.</p>',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
